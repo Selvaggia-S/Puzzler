d53944cd96b322c3b75b0786828797c78082ce54
Selvaggia-S <33578819+Selvaggia-S@users.noreply.github.com>
2017-11-11 19:02:42 +0100

    Initial commit

---
+++ Assets/GoogleVR/Scripts/orbsRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbsRotation : MonoBehaviour
+{
+
+    // Use this for initialization    
+    public float speed = 30;
+    public float xRange = 20;
+    public float yRange = 4;
+    Vector3 vector;
+
+
+    void Update()
+    {
+        vector = new Vector3(Random.Range(-1 * xRange, xRange), Random.Range(-1 * yRange, yRange), 0f);
+        transform.Rotate(vector * Time.deltaTime);
+    }
+}
